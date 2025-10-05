621a084cca1000d03526b32a0312be1f3ab9a7e1
AQIBMOHD <aqibmohammad.0002@gmail.com>
2025-10-06 04:49:54 +0530

    Updated login UI, guards, and admin dashboard improvements

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+ imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -16,7 +23,7 @@ export class LoginComponent implements OnInit {
   password: string = '';
   errorMessage: string = '';
   isLoading: boolean = false;
-  
+
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
